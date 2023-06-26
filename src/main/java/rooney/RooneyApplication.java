74819817c51ae80666ba3765ea462f9bdac5ab64
shghdrl1234 <shghdrl1234@gmail.com>
2023-06-26 23:43:45 +0900

    feat : CICD test

---
+++ src/main/java/rooney/RooneyApplication.java
@@ -0,0 +1,13 @@
+package rooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RooneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RooneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rooney/RooneyApplicationTests.java
@@ -0,0 +1,13 @@
+package rooney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RooneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
